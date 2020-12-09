cbf5eb32e0d3366f4b19b06df692645123faaa9d
zzk930417 <1165717109@qq.com>
2020-12-09 23:01:50 +0800

    代码删除

--- app/src/main/java/com/test/eurekaClient/common/UrlUtil.java
+++
@@ -1,9 +0,0 @@
-package com.test.eurekaClient.common;
-
-public class UrlUtil {
-
-	public static final String TESTURL = BaseUtil.DOMAIN+"/eureka-domain/test/getname?name={name}";
-	
-	public static final String TESTURL_GETLIST = BaseUtil.DOMAIN+"/eureka-domain/test/getList";
-	
-}
