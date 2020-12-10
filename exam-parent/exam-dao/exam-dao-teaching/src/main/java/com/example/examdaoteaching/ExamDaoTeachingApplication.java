78ca5d6a7aaa9aaddae9e4cd95f1f3ce89dc4c5a
zhangzhenkun <zhangzhenkun@sinosoft.com.cn>
2020-12-10 14:12:43 +0800

    代码测试提交

---
+++ exam-parent/exam-dao/exam-dao-teaching/src/main/java/com/example/examdaoteaching/ExamDaoTeachingApplication.java
@@ -0,0 +1,13 @@
+package com.example.examdaoteaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamDaoTeachingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamDaoTeachingApplication.class, args);
+    }
+
+}
---
+++ exam-parent/exam-dao/exam-dao-teaching/src/test/java/com/example/examdaoteaching/ExamDaoTeachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examdaoteaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamDaoTeachingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
